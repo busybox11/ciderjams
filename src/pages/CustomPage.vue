4bd35cb78340e51efe1f7029e0c621f0a7e56317
busybox11 <busybox11th@gmail.com>
2025-10-01 00:58:51 +0200

    chore(deps): bump vue and related deps

--- src/pages/CustomPage.vue
+++ src/pages/CustomPage.vue
@@ -3,31 +3,28 @@ import { DialogAPI, useCider } from "@ciderapp/pluginkit";
 import { computed } from "vue";
 import HelloWorld from "../components/HelloWorld.vue";
 
-
 const openComponentDialog = () => {
   DialogAPI.createDialog({
-    component: 'plugin-template-component-based-modal',
+    component: "plugin-template-component-based-modal",
   }).show();
 };
 
 const cfg = computed(() => {
   return useCider().config.getRef();
 });
-
 </script>
 
 <template>
   <div class="q-pa-lg plugin-base">
-    <h1 class="apple-heading">My Custom Plugin Page</h1>
+    <h1 class="apple-heading">Cider Jams</h1>
     {{ cfg.general.displayName }}
 
     <p>This is a custom page. You can add your own content here.</p>
     <HelloWorld></HelloWorld>
 
-    <button
-      class="c-btn"
-      @click="openComponentDialog"
-    >Show component based modal</button>
+    <button class="c-btn" @click="openComponentDialog">
+      Show component based modal
+    </button>
   </div>
 </template>
 
