2c554739b4066a445a3351cf2ad411e8f061ebdf
busybox11 <busybox11th@gmail.com>
2026-05-10 23:51:57 +0200

    feat(proto): add schema defs with types

---
+++ proto/src/payloads.ts
@@ -0,0 +1,138 @@
+/**
+ * The following events will be used to communicate from a client to the server:
+ *
+ * - ping
+ * - room.create
+ * - room.join
+ * - room.leave
+ * - queue.set
+ * - player.play
+ * - player.pause
+ * - player.seek
+ * - player.next
+ * - player.previous
+ * - player.setRepeat
+ * - player.setShuffle
+ *
+ *
+ * The following events will be used to communicate from the server to the client:
+ *
+ * - ping
+ * - room.state
+ * - queue.state
+ * - player.state
+ *
+ * We define payloads using Zod schemas, which will also export type definitions for the payloads.
+ */
+
+import * as z from "zod";
+import {
+  playerRepeatMode,
+  playerShuffleMode,
+  queueEntry,
+  roomMeta,
+  roomParticipant,
+  roomPlaybackState,
+} from "./room";
+
+export const pingPayload = z.object({});
+export type PingPayload = z.infer<typeof pingPayload>;
+
+export const roomCreatePayload = z.object({
+  user: roomParticipant,
+});
+export type RoomCreatePayload = z.infer<typeof roomCreatePayload>;
+
+export const roomJoinPayload = z.object({
+  roomCode: roomMeta.shape.roomCode,
+});
+export type RoomJoinPayload = z.infer<typeof roomJoinPayload>;
+
+export const roomLeavePayload = z.object({});
+export type RoomLeavePayload = z.infer<typeof roomLeavePayload>;
+
+export const queueSetPayload = z.array(
+  queueEntry.extend({
+    queueEntryId: queueEntry.shape.queueEntryId
+      .optional()
+      .describe("Unset when a new track is added to the queue"),
+    ownerUserId: queueEntry.shape.ownerUserId
+      .optional()
+      .describe("Unset when a new track is added to the queue"),
+  }),
+);
+export type QueueSetPayload = z.infer<typeof queueSetPayload>;
+
+export const playerPlayPayload = z.object({});
+export type PlayerPlayPayload = z.infer<typeof playerPlayPayload>;
+
+export const playerPausePayload = z.object({});
+export type PlayerPausePayload = z.infer<typeof playerPausePayload>;
+
+export const playerSeekPayload = z.object({
+  positionMs: z.number(),
+});
+export type PlayerSeekPayload = z.infer<typeof playerSeekPayload>;
+
+export const playerNextPayload = z.object({});
+export type PlayerNextPayload = z.infer<typeof playerNextPayload>;
+
+export const playerPreviousPayload = z.object({});
+export type PlayerPreviousPayload = z.infer<typeof playerPreviousPayload>;
+
+export const playerSetRepeatPayload = z.object({
+  repeatMode: playerRepeatMode,
+});
+export type PlayerSetRepeatPayload = z.infer<typeof playerSetRepeatPayload>;
+
+export const playerSetShufflePayload = z.object({
+  shuffleMode: playerShuffleMode,
+});
+export type PlayerSetShufflePayload = z.infer<typeof playerSetShufflePayload>;
+
+export const clientEventPayloads = {
+  ping: pingPayload,
+  "room.create": roomCreatePayload,
+  "room.join": roomJoinPayload,
+  "room.leave": roomLeavePayload,
+  "queue.set": queueSetPayload,
+  "player.play": playerPlayPayload,
+  "player.pause": playerPausePayload,
+  "player.seek": playerSeekPayload,
+  "player.next": playerNextPayload,
+  "player.previous": playerPreviousPayload,
+  "player.setRepeat": playerSetRepeatPayload,
+  "player.setShuffle": playerSetShufflePayload,
+} as const;
+
+export type ClientEvent = keyof typeof clientEventPayloads;
+
+export type ClientEventPayloadMap = {
+  [K in ClientEvent]: z.infer<(typeof clientEventPayloads)[K]>;
+};
+
+export const roomStateSchema = roomMeta.omit({
+  state: true,
+});
+export type RoomStateSchema = z.infer<typeof roomStateSchema>;
+
+export const queueStateSchema = z.array(queueEntry);
+export type QueueStateSchema = z.infer<typeof queueStateSchema>;
+
+export const playerStateSchema = roomPlaybackState.omit({
+  queue: true,
+});
+export type PlayerStateSchema = z.infer<typeof playerStateSchema>;
+
+export const serverEventPayloads = {
+  ping: pingPayload,
+  "room.state": roomStateSchema,
+  "queue.state": queueStateSchema,
+  "player.state": playerStateSchema,
+} as const;
+
+export type ServerEvent = keyof typeof serverEventPayloads;
+
+export type ServerEventPayloadMap = {
+  [K in ServerEvent]: z.infer<(typeof serverEventPayloads)[K]>;
+};
---
+++ proto/src/room.ts
@@ -0,0 +1,59 @@
+import * as z from "zod";
+
+/* MusicKit like shapes */
+export const CatalogItemId = z.string();
+export const LibraryItemId = z.string();
+
+export const playbackState = z.enum([
+  "PREVIEW_ONLY",
+  "MIXED_CONTENT",
+  "FULL_PLAYBACK_ONLY",
+  "SHAREPLAY_PARTICIPANT",
+]);
+export type playbackState = z.infer<typeof playbackState>;
+
+export const playerRepeatMode = z.enum([
+  "REPEAT_ALL",
+  "REPEAT_OFF",
+  "REPEAT_ONE",
+]);
+export type PlayerRepeatMode = z.infer<typeof playerRepeatMode>;
+
+export const playerShuffleMode = z.enum(["SHUFFLE_OFF", "SHUFFLE_ON"]);
+export type PlayerShuffleMode = z.infer<typeof playerShuffleMode>;
+
+/* Internal shapes */
+export const queueEntry = z.object({
+  queueEntryId: z.string().describe("Server-generated internal queue item ID"),
+  ownerUserId: z.string(),
+  itemCatalogId: CatalogItemId,
+});
+export type queueEntry = z.infer<typeof queueEntry>;
+
+export const roomPlaybackState = z.object({
+  queue: z.array(queueEntry),
+  currentPlayingIndex: z.int(),
+  elapsedTimeMs: z.number(),
+  playbackState: playbackState,
+  repeatMode: playerRepeatMode,
+  shuffleMode: playerShuffleMode,
+  autoPlay: z.boolean(),
+  updatedAtMs: z.int(), // should probably use better time primitives, will figure out later
+});
+export type RoomPlaybackState = z.infer<typeof roomPlaybackState>;
+
+export const roomParticipant = z.object({
+  userId: z.string().describe("Apple Music social profile user ID"),
+  name: z.string(),
+  handle: z.string(),
+  avatar: z.url(),
+});
+export type roomParticipant = z.infer<typeof roomParticipant>;
+
+export const roomMeta = z.object({
+  roomId: z.string().describe("Server-generated internal room ID"),
+  roomCode: z.string().describe("User-friendly room joining code"), // should probably use stricter zod-defined shapes instead
+  hostUserId: roomParticipant.shape.userId,
+  state: roomPlaybackState,
+});
+export type roomMeta = z.infer<typeof roomMeta>;
