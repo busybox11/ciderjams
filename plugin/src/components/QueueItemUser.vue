168153a5d51040b6cbfa66bea2b8cdbac26c4eff
busybox11 <busybox11th@gmail.com>
2026-05-15 18:21:48 +0200

    feat(plugin/QueueItem): show proper item owner id data

--- plugin/src/components/QueueItemUser.vue
+++ plugin/src/components/QueueItemUser.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
 import { useJamStore } from "../stores/main";
 
-defineProps<{
+const props = defineProps<{
   item: MusicKit.MediaItem;
 }>();
 
 const jamStore = useJamStore();
 
+const currentQueueState = computed(() => jamStore.lastQueueState);
+
 const currentJam = computed(() => jamStore.currentJam);
+const jamTrack = computed(() => {
+  if (!currentQueueState.value) return null;
+  const ownerId = currentQueueState.value.find((q) => q.itemCatalogId === props.item.id)?.ownerUserId;
+  if (!ownerId) return null;
+  return currentJam.value?.participants.find((p) => p.userId === ownerId);
+});
 </script>
 
 <template>
-  <div class="queue-item-user" v-if="currentJam">
-    <img src="https://cdn.discordapp.com/avatars/750770863418376216/f20db21adc47f1c0d92bba4042ae8aef.webp?size=240" alt="Member 3" style="width: 1.5rem; height: 1.5rem; border-radius: 50%; display: flex; margin-left: 0.5rem;" />
+  <div class="queue-item-user" v-if="jamTrack">
+    <img :src="jamTrack.avatar" alt="Member 3" style="width: 1.5rem; height: 1.5rem; border-radius: 50%; display: flex; margin-left: 0.5rem;" />
   </div>
 </template>
 
